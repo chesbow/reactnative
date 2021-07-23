d1e1be356dc3dff56a466b6a41f0f3c399e4e7ff
Chi Bao <1510169@hcmute.edu.vn>
2021-07-23 09:10:08 +0700

    giao lan 1

---
+++ customer/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Customer
+from rest_framework import generics
+from .serializers import CustomerSerializer
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello nha!!!!!!")
+
+def homepage(request):
+  return render(request, 'index.html', context={})
+
+class CustomerCreate(generics.CreateAPIView):
+    # API endpoint that allows creation of a new customer
+    queryset = Customer.objects.all(),
+    serializer_class = CustomerSerializer
+
+
+class CustomerList(generics.ListAPIView):
+    # API endpoint that allows customer to be viewed.
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class CustomerDetail(generics.RetrieveAPIView):
+    # API endpoint that returns a single customer by pk.
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class CustomerUpdate(generics.RetrieveUpdateAPIView):
+    # API endpoint that allows a customer record to be updated.
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class CustomerDelete(generics.RetrieveDestroyAPIView):
+    # API endpoint that allows a customer record to be deleted.
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
